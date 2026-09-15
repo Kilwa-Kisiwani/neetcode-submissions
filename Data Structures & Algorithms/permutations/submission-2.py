1f4b7ff86e5382fa465d4f786a1d02e08c009ae4
Eric Zhang <bz52@uw.edu>
2026-09-14 19:24:04 -0700

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return []
+        res = []
+        available = set(nums)
+        self.helper(available, [], res)
+        return res
+
+
+
+    def helper(self, nums: Set[int], curr: List[int], res: List[List[int]]):
+        if len(nums) == 0:
+            res.append(curr.copy())
+            return
+        nums_copy = nums.copy()
+        for num in nums:
+            nums_copy.remove(num)
+            curr.append(num)
+            self.helper(nums_copy, curr, res)
+            nums_copy.add(num)
+            curr.remove(num)
+
+
+            
